6d28ec49a7c19d723c989a4a64ce3cb13716fb9a
Rodrigo Horning <rodrigo_salvia@hotmail.com>
2017-06-17 16:02:52 -0300

    Improved configureMonitor and monitoring pages

--- Server.Nodejs/myapp/routes/index.js
+++ Server.Nodejs/myapp/routes/index.js
@@ -9,17 +9,20 @@ router.get('/', function(req, res, next) {
 
 router.get('/configureMonitorSensor', function(req, res, next) {
   dbHandler.getAllSensorsOnDB(function(sensors) {
-    res.render('configureMonitorSensor', {title: 'SmartSensing', sensors: sensors});
+    var nMeasures = [5, 10, 15, 20, 25];
+    res.render('configureMonitorSensor', {title: 'SmartSensing', sensors: sensors, nMeasures: nMeasures});
   });
 });
 
 router.post('/monitoring', function(req, res, next) {
+  var maxValue = req.body.maxValue;
+  var minValue = req.body.minValue;
+  var nMeasures = parseInt(req.body.nMeasures);
   var sensor = req.body.sensor;
   array = sensor.split('-');
   var code = array[0];
-  dbHandler.getLastMeasuresOnDB(code, 15, function(measures, timeLogs) {
-    var timeLogsTxt = timeLogs.toString().split(',');
-    res.render('monitoring', {title: 'SmartSensing', sensor: code, measures: measures, timeLogs: timeLogs});
+  dbHandler.getLastMeasuresOnDB(code, nMeasures, function(measures, timeLogs) {
+    res.render('monitoring', {title: 'SmartSensing', sensor: code, measures: measures, nMeasures: nMeasures, timeLogs: timeLogs, maxValue: maxValue, minValue: minValue});
   });
 });
 
